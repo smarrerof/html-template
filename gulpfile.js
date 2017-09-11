1f98f8178a323b2b54769cd170ad1294ea15a48c
smarrerof <smarrerof@gmail.com>
2017-09-11 06:28:33 +0100

    Added scss support

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
 
 
 gulp.task('hello', function() {
@@ -9,15 +10,24 @@ gulp.task('hello', function() {
 
 
 // Static server + watching files
-gulp.task('serve', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./src"
-        }
-    });
+gulp.task('serve', ['sass'], function() {
+  browserSync.init({
+    server: {
+      baseDir: "./src"
+    }
+  });
 
-    // Watchers
-    gulp.watch("src/**/*.html").on('change', browserSync.reload);
+  // Watchers
+  gulp.watch("src/assets/css/**/*.scss", ['sass']);
+  gulp.watch("src/**/*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+  return gulp.src("src/assets/css/**/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("src/assets/css"))
+    .pipe(browserSync.stream());
 });
 
 
